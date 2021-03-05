325a9d0766077b19161ddaca96988cb369ef4a60
Yusuke Matsui <matsui528@gmail.com>
2021-03-05 14:29:20 +0900

    sanity check only for annoy

--- sanity_check.sh
+++ sanity_check.sh
@@ -1,3 +1,7 @@
+# Should be run at "./annbench"
+
+echo "## Run with a conda container" >> report.md
+
 python run.py algo=annoy dataset=siftsmall "algo.param_index=\
 [\
     {n_trees:100},\
@@ -6,3 +10,7 @@ python run.py algo=annoy dataset=siftsmall "algo.param_index=\
     {n_trees:800},\
     {n_trees:1600}\
 ]"
+
+python plot.py
+
+cml-publish ./annbench/result_img/siftsmall.png --md >> report.md
